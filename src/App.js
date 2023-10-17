1ad2df9e42cf4c3c34435cb81981901b29dfc0cf
saeedjhn <saeedjahanakbari@gmail.com>
2023-10-17 13:46:33 +0330

    Init: Building & structuring workspace

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <p className="paragraph">
+          Labore cillum aliquip laboris laboris cupidatat cupidatat et
+          exercitation.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
